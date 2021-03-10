89ee38d200240c5db2fafcb1589b06f955d881c7
MosesSU <18758208937@163.com>
2021-03-10 18:17:49 +0800

    Flask-SQLAlchemy简介和创建用户模型

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    # Database configuration
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + "E:/Moses/College_Life/Year3_2/Software_Development_Workshop_III/Code/practice2/app.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
