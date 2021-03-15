fb6de8fb373bfe66012c6eb87cc37edeabf56318
MosesSU <18758208937@163.com>
2021-03-15 21:25:52 +0800

    Flask reCAPTCHA support 是否是机器人测试（未实现）

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
@@ -8,4 +8,5 @@ class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
     confirm = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+    # recaptcha = RecaptchaField()
     submit = SubmitField('Register')
