8171a8721635c331120d3e09ee59b1e599dd0362
MosesSU <18758208937@163.com>
2021-03-15 21:53:31 +0800

    利用Flask-Bcrypt来加密数据

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 from config import Config
 from forms import RegisterForm
+from models import User
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app=app)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 app.config.from_object(Config)
 
@@ -29,8 +32,13 @@ def register():
     if form.validate_on_submit():
         username = form.username.data
         email = form.email.data
-        password = form.password.data
+        password = bcrypt.generate_password_hash(form.password.data)
         print(username, email, password)
+        # print(bcrypt.check_password_hash(hash, password))
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congrats, registration success', category='success')
     return render_template('register.html', form=form)
 
 
