12a2229b586740c6dc81d8fd9a57246ad4d672fe
MosesSU <18758208937@163.com>
2021-03-16 14:59:53 +0800

    判断用户名和邮箱是否在数据库中并显示flash和注册成功后的重定向

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+from app.models import User
 
 
 class RegisterForm(FlaskForm):
@@ -10,3 +12,18 @@ class RegisterForm(FlaskForm):
     confirm = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
     # recaptcha = RecaptchaField()
     submit = SubmitField('Register')
+
+    # The way to name the function must be validate_xxx. And xxx here must be a variable in RegisterForm class
+    def validate_username(self, username):
+        print(username.data)
+        print(User.query.filter_by(username=username.data).first())
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken, please choose another one.')
+
+    def validate_email(self, email):
+        # print(email.data)
+        # print(User.query.filter_by(username=email.data).first())
+        user = User.query.filter_by(username=email.data).first()
+        if user:
+            raise ValidationError('Email already taken, please choose another one.')
